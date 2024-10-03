eb51062377dfaeb518363702eda4c99abd3f0b7a
Stud302 <Stud302@COMP302-09>
2024-10-03 12:32:45 +0300

    Доделано задание

---
+++ 1/ClassPerson/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPerson
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public string Address;
+        public Person(string name, int age, string address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+            public void Print()
+            {
+            Console.WriteLine("Имя: {0} \nВозраст: {1} \nАдрес: {2}",Name, Age, Address);
+        }
+        
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Person person = new Person("Даниил",17,"1 мая");
+            person.Print();
+            Console.ReadLine();
+        }
+    }
+}
